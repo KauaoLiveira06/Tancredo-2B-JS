d5cb37d02fb78b41ae49a00a79172019b865f189
Kauã dos Reis de Oliveira <127523958+KauaoLiveira06@users.noreply.github.com>
2023-09-04 10:47:29 -0300

    Aula 04/09 JavaScript

---
+++ main.js
@@ -0,0 +1,17 @@
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+function tocaSomSplash () {
+    document.querySelector('#som_tecla_splash').play();
+}
+
+document.querySelector('.tecla_splash').onclick = tocaSomSplash;
+
+function tocaSomTim () {
+    document.querySelector('#som_tecla_tim').play();
+}
+
+document.querySelector('.tecla_tim').onclick = tocaSomTim;
